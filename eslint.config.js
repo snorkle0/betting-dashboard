ed2953ed494e2a171f95593d4f9ee9f99279c78f
snorkle0 <snorkle0@gmail.com>
2025-05-01 10:06:41 +0200

    task: cleanup configuration files

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-import pluginRouter from '@tanstack/eslint-plugin-router';
+import js from '@eslint/js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
-
-
+import pluginRouter from '@tanstack/eslint-plugin-router';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
   ...tseslint.configs['recommended'],
   ...pluginReact.configs.flat['recommended'],
   ...pluginRouter.configs['flat/recommended'],
-  ...pluginQuery.configs['flat/recommended']
+  ...pluginQuery.configs['flat/recommended'],
 ]);
